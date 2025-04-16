a565d596639fc9a874678b3ec24fd6a497edad01
TheMathGeek_314 <16294237+TheMathGeek314@users.noreply.github.com>
2025-04-16 16:19:51 -0500

    Add project files.

---
+++ ReadMe.md
@@ -0,0 +1,16 @@
+# ShuffledCharmEffects
+
+This mod changes around the effects of every vanilla charm. The shuffling is completely random and is chosen when a savefile is first created.
+Opening an existing unrandomized file will retain vanilla charm effects.
+All effects and synergies of any charm will be demonstrated by the mimic charm and not by its original source.
+Names, locations, and notch costs remain vanilla, only the effects are shuffled.
+
+Some quirks of specific charms include:
+- If you die while wearing a fragile charm, it won't break
+- If you die while wearing a charm that is imitating a fragile charm:
+	- The fragile being imitated will break and become unequippable
+	- The equipped charm will stay on and can be equipped freely, but it will have no effect until repaired
+- The first three grimmkin flames can be collected regardless of whether the troupe has been summoned
+- The charm that imitates Kingsoul can be equipped at any time but will have no effect until both fragments have been obtained
+
+*Special thanks to BirdiestBlue for commissioning this mod*
---
+++ ShuffledCharmEffects.cs
@@ -0,0 +1,83 @@
+﻿using Modding;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using HutongGames.PlayMaker;
+using Satchel;
+
+namespace ShuffledCharmEffects {
+    public class ShuffledCharmEffects: Mod, ILocalSettings<LocalSettings> {
+        new public string GetName() => "ShuffledCharmEffects";
+        public override string GetVersion() => "1.0.0.0";
+
+        public static LocalSettings localData { get; set; } = new();
+        public void OnLoadLocal(LocalSettings s) => localData = s;
+        public LocalSettings OnSaveLocal() => localData;
+
+        public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects) {
+            On.PlayerData.GetBool += getBoolRecharm;
+            On.PlayMakerFSM.OnEnable += editFsm;
+            On.GameManager.StartNewGame += initialSetup;
+        }
+
+        private bool getBoolRecharm(On.PlayerData.orig_GetBool orig, PlayerData self, string boolName) {
+            if(boolName.StartsWith("equippedCharm_")) {
+                if(!Environment.StackTrace.Contains("CalculateNotchesUsed")) {
+                    int origID = int.Parse(boolName.Split('_')[1]);
+                    boolName = "equippedCharm_" + localData.shuffledIDs[origID - 1];
+                }
+            }
+            return orig(self, boolName);
+        }
+
+        private void editFsm(On.PlayMakerFSM.orig_OnEnable orig, PlayMakerFSM self) {
+            orig(self);
+            if(self.gameObject.name == "Charms" && self.FsmName == "UI Charms") {
+                FsmState equippedState = self.GetValidState("Equipped?");
+                equippedState.RemoveAction(1);
+                equippedState.InsertAction(new ReplaceBuildString(self), 1);
+            }
+        }
+
+        private void initialSetup(On.GameManager.orig_StartNewGame orig, GameManager self, bool permadeathMode, bool bossRushMode) {
+            orig(self, permadeathMode, bossRushMode);
+            localData.shuffledIDs = createIDs();
+        }
+
+        private List<int> createIDs() {
+            System.Random rand = new();
+            List<int> numbers = new();
+            List<int> output = new();
+            for(int i = 1; i <= 40; i++) {
+                numbers.Add(i);
+            }
+            while(numbers.Count > 0) {
+                int index = rand.Next(numbers.Count);
+                output.Add(numbers[index]);
+                numbers.RemoveAt(index);
+            }
+            return output;
+        }
+    }
+
+    public class ReplaceBuildString: FsmStateAction {
+        PlayMakerFSM fsm;
+
+        public ReplaceBuildString(PlayMakerFSM self) {
+            fsm = self;
+        }
+
+        public override void OnEnter() {
+            string fsmString = fsm.FsmVariables.GetFsmString("Item Num String").Value;
+            int revertedID = ShuffledCharmEffects.localData.shuffledIDs.IndexOf(int.Parse(fsmString)) + 1;
+            fsm.FsmVariables.GetFsmString("PlayerData Var Name").Value = "equippedCharm_" + revertedID;
+        }
+    }
+
+    public class LocalSettings {
+        public List<int> shuffledIDs = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                                                       11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+                                                       21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+                                                       31, 32, 33, 34, 35, 36, 37, 38, 39, 40};
+    }
+}
